a30584681cceb43d0df82ade5b7538595a575e7d
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2024-08-14 15:37:52 -0300

    Atualização da Condicional em Float

--- codigos/condicional_float_120824.c
+++ codigos/condicional_float_120824.c
@@ -6,20 +6,24 @@
     LP - Jogos Digitais
 
 */
+//Inclusão das bibliotecas que trabalham com caracteres, números e operações matemáticas
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+//Função principal da classe
 int main()
 {
+    //Indico a vari~´avel, seu tipo e o limite de dados
     float n;
     printf ("Informe um número: ");
     scanf ("%f", &n);
     
     /* Pensando no par ou impar */
-    if (fmod(n, 2) == 0) {
-        printf ("%f eh par. O resto da divisao eh %f", n, fmod(n, 2));
+    if (fmod(n, 2) == 0 || fmod(n, 2) < 1) {
+        printf ("%2f eh par. O resto da divisao eh %2f", n, fmod(n, 2));
     } else {
-        printf ("%f eh impar. O resto da divisao eh %f", n, fmod(n, 2));
+        printf ("%2f eh impar. O resto da divisao eh %2f", n, fmod(n, 2));
     }
 
     return 0;
